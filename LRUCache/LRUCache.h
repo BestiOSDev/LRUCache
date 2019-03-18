53fb505044b8ac7ffa52e6dfaf3c1ca2aed4216c
dong <dzb8818082@163.com>
2019-03-18 20:48:37 +0800

    ddd

--- LRUCache/LRUCache.h
+++ LRUCache/LRUCache.h
@@ -2,53 +2,84 @@
 //  LRUCache.h
 //  LRUCache
 //
-//  Created by dzb on 2019/3/15.
-//  Copyright © 2019 大兵布莱恩特. All rights reserved.
+//  Created by dzb on 2019/3/18.
+//  Copyright © 2019年 大兵布莱恩特. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface LRUCache  <KeyType, ObjectType> : NSObject
+@protocol LRUCacheDelegate;
 
-///shareCache
+@interface LRUCache : NSObject
+
 @property (nonatomic,strong,class) LRUCache *shareCache;
-
 /**
- 存储数据
-
- @param object 对象
- @param aKey key
+ 是否包含key
  */
-- (void) setObject:(ObjectType)object forKey:(KeyType)aKey;
+- (BOOL)containsObjectForKey:(id)key;
 
 /**
- 获取存储的数据
-
- @param aKey 通过 key 查找
- @return 返回数据
+ 根据键取对应值
  */
-- (nullable ObjectType) objectForKey:(KeyType)aKey;
+- (nullable id)objectForKey:(id)key;
 
 /**
- 移除存储的数据
-
- @param aKey 通过 key 方法
+ 通过键值来存储数据
  */
-- (void) removeObjectForKey:(KeyType)aKey;
+- (void)setObject:(nullable id)object forKey:(id)key;
 
 /**
- 移除所有缓存数据
+ 删除缓存中指定键的值。
+ 
+ key标识要删除的值的键。如果为nil，则此方法无效。
+ */
+- (void)removeObjectForKey:(id)key;
+
+/**
+ Empties the cache immediately.
  */
 - (void)removeAllObjects;
 
+/** The number of objects in the cache (read-only) */
+@property (readonly) NSUInteger totalCount;
+
+#pragma mark - Limit
+
 /**
- 缓存数量限制 默认是10 当重新设置改值时 旧的缓存会被清空
+ default is 10 最大缓存10个对象
  */
 @property (nonatomic,assign) NSUInteger countLimit;
 
+/**
+ 间隔多少秒检查缓存情况 默认5秒间隔
+ */
+@property NSTimeInterval autoTrimInterval;
+
+/**
+ 委托对象
+ */
+@property (nonatomic,weak) id<LRUCacheDelegate>delegate;
+
+/**
+ 收到内存警告时 是否清理缓存
+ */
+@property (nonatomic,assign) BOOL shouldRemoveAllObjectsOnMemoryWarning;
 
 @end
 
 NS_ASSUME_NONNULL_END
+
+
+@protocol LRUCacheDelegate <NSObject>
+
+/**
+ 每次被淘汰的缓存 就会调用这个方法
+
+ @param lru 缓存类
+ @param obj 将要被淘汰的缓存对象
+ */
+- (void) lurcache:(LRUCache *_Nullable)lru willEvictObject:(id _Nullable )obj;
+
+@end
